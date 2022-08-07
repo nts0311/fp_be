8afd821da467ed0d83bb0abfc93887c9e866f06d
nts0311 <ngothaison12g@gmail.com>
2022-08-07 11:39:57 +0700

    init project

---
+++ src/main/kotlin/com/sonnt/fp_be/FpBeApplication.kt
@@ -0,0 +1,11 @@
+package com.sonnt.fp_be
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FpBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<FpBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/sonnt/fp_be/FpBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sonnt.fp_be
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FpBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
